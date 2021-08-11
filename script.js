2fd33b8bab8ef3573744d2c51afea06bc3ec8244
Pasindu Senanayake <pasindusenanayake@icloud.com>
2021-08-11 02:00:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function myfunction(btn){
+if(btn == "btn1")
+  {
+    document.getElementById("para").innerHTML = "Online shopping is a form of electronic commerce which allows consumers to directly buy goods or services from a seller over the Internet using a web browser or a mobile app. Consumers find a product of interest by visiting the website of the retailer directly or by searching among alternative vendors using a shopping search engine, which displays the same product's availability and pricing at different e-retailers. As of 2020, customers can shop online using a range of different computers and devices, including desktop computers, laptops, tablet computers and smartphones.";
+    document.getElementById("img1").src = "images/cart.png";
+  }
+  else if(btn == "btn2")
+  {
+    document.getElementById("para").innerHTML = "The domestic dog (Canis familiaris or Canis lupus familiaris)[4] is a domesticated form of wolf. The dog descended from an ancient, extinct wolf,[5][6] with the modern wolf being the dog's nearest living relative.[7] The dog was the first species to be domesticated[8][7] by hunter–gatherers more than 15,000 years ago,[6] prior to the development of agriculture.[1] Their long association with humans has led dogs to be uniquely attuned to human behavior,[9] enabling an abundant cosmopolitan distribution[10] and to be able to thrive on a starch-rich diet that would be inadequate for other canids.";
+    document.getElementById("img1").src = "images/Dog.jpg";
+  }
+  else if(btn == "btn3")
+  {
+    document.getElementById("para").innerHTML = "The cat (Felis catus) is a domestic species of small carnivorous mammal.[1][2] It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.[4] A cat can either be a house cat, a farm cat or a feral cat; the latter ranges freely and avoids human contact.[5] Domestic cats are valued by humans for companionship and their ability to hunt rodents. About 60 cat breeds are recognized by various cat registries.";
+    document.getElementById("img1").src = "images/Cat.jpg";
+  }
+  else if(btn == "btn4")
+  {
+    document.getElementById("para").innerHTML = "Fish are aquatic, craniate, gill-bearing animals that lack limbs with digits. They form a sister group to the tunicates, together forming the olfactores. Included in this definition are the living hagfish, lampreys, and cartilaginous and bony fish as well as various extinct related groups. Around 99% of living fish species are ray-finned fish, belonging to the class Actinopterygii, with over 95% belonging to the teleost subgrouping.";
+    document.getElementById("img1").src = "images/Fish.jpg";
+  }
+  else if(btn == "btn5")
+  {
+    document.getElementById("para").innerHTML = "Rabbits are small mammals in the family Leporidae (along with the hare) of the order Lagomorpha (along with the pika). Oryctolagus cuniculus includes the European rabbit species and its descendants, the world's 305 breeds[1] of domestic rabbit. Sylvilagus includes 13 wild rabbit species, among them the seven types of cottontail. The European rabbit, which has been introduced on every continent except Antarctica, is familiar throughout the world as a wild prey animal and as a domesticated form of livestock and pet.";
+    document.getElementById("img1").src = "images/Rabbit.jpg"; 
+  }
+  else if(btn == "btn6")
+  {
+    document.getElementById("img1").src = "images/Cow.jpg";
+    document.getElementById("para").innerHTML = "Cattle are commonly raised as livestock for meat (beef or veal, see beef cattle), for milk (see dairy cattle), and for hides, which are used to make leather. They are used as riding animals and draft animals (oxen or bullocks, which pull carts, plows and other implements). Another product of cattle is their dung, which can be used to create manure or fuel. In some regions, such as parts of India, cattle have significant religious meaning. Cattle, mostly small breeds such as the Miniature Zebu, are also kept as pets.";
+  }
+}
+
